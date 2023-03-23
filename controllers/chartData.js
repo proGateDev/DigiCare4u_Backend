f965654b760498b25b6f76ce94aa11dd998f309b
Nischal <gupta.nischal014@gmail.com>
2023-03-23 15:19:57 +0530

    fixes the chart issues

--- controllers/chartData.js
+++ controllers/chartData.js
@@ -17,41 +17,43 @@ module.exports = {
     );
     let finedData = chartData?.data["Time Series (Daily)"];
 
-    // let d = finalData.map((x) => {
-    //   return x["8. split coefficient"];
-    // });
     const dataArray = Object.entries(finedData).map(([date, values]) => ({
       date,
       ...values,
     }));
 
     const planet = await model.find({ planet: req.query.planet });
-
+    const planetSign = req.query.sign;
     const now = dayjs();
     const month = now.month();
 
     const planetTransit = planet[0].transit;
     const data = planetTransit.find((w) => {
-      if (dayjs(w.date).month() == month) return w;
+      if (w.zodiac === planetSign) return w;
     });
 
-    if (typeof data === "undefined" || data.length == 0) {
-      return res
-        .status(404)
-        .json(`No transit this month for ${req.query.planet}`);
-    }
+    // if (typeof data === "undefined" || data.length == 0) {
+    //   return res
+    //     .status(404)
+    //     .json(`No transit this month for ${req.query.planet}`);
+    // }
     //---------------------------------------------
     const index = planetTransit.indexOf(data);
     let secondIndex = index - 1;
     //---------------------------------------------
     let from = planetTransit[secondIndex]?.date;
-    let to = data.date;
+    let to = planetTransit[index]?.date;
 
     let finaleData = dataArray.filter((x) => {
       if (x.date <= to && x.date >= from) {
         return x;
       }
     });
+
+    // console.log("Index", index);
+    // console.log("secondIndex", secondIndex);
+    console.log("finaleData", finaleData);
+
     //---------------------------------------------
     res.status(200).json(finaleData.reverse());
   },

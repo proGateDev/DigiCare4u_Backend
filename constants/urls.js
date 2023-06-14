b440b0081c40620d6dc4756f9c569ff90f2cff51
Nischal <gupta.nischal014@gmail.com>
2023-06-14 17:34:49 +0530

    User creations with all the natal details-- API

--- constants/urls.js
+++ constants/urls.js
@@ -1,5 +1,5 @@
-// const ephemerisApi = "https://ephemeris.onrender.com"
-const ephemerisApi = "http://127.0.0.1:5000"
+const ephemerisApi = "https://ephemeris.onrender.com"
+// const ephemerisApi = "http://127.0.0.1:5000"
 
 
 module.exports = {ephemerisApi} 

96802b105555c60b392bbc6ecea4fc52c42872a0
nischal <gupta.nischal014@gmail.com>
2024-01-03 23:02:34 +0530

    commit

--- controllers/almanac.js
+++ controllers/almanac.js
@@ -6,18 +6,25 @@ const { almanacData } = require('../utils/astro')
 module.exports = {
   almanac: async (req, res) => {
     try {
+      console.log('======================================= started');
 
       const date = req.body.date
       const time = req.body.time
-      let ccc = almanacData(date, time)
+      if (!date) {
+        res.status(404).json({ 'message': 'provide date' });
 
-      jsonResponse = {
-        status: 200,
-        message: "Planetary position for a given date & time",
-        data: await ccc,
-      };
+      } else {
 
-      res.status(200).json(jsonResponse);
+        let ccc = almanacData(date, time)
+
+        jsonResponse = {
+          status: 200,
+          message: "Planetary position for a given date & time",
+          data: await ccc,
+        };
+
+        res.status(200).json(jsonResponse);
+      }
     } catch (error) {
       console.error("Error fetching user data:", error);
       res.status(500).json({ error: "Internal Server Error" });

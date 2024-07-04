80aa2b5621ca320cd8ac3b7ec960d48dff56d19b
Nischal Gupta <nischal@onecorp.co.in>
2024-07-04 19:04:01 +0530

    cim

--- controllers/chartData.js
+++ controllers/chartData.js
@@ -17,44 +17,45 @@ module.exports = {
     );
     let finedData = chartData?.data["Time Series (Daily)"];
 
-    const dataArray = Object.entries(finedData).map(([date, values]) => ({
-      date,
-      ...values,
-    }));
+    // const dataArray = Object.entries(finedData).map(([date, values]) => ({
+    //   date,
+    //   ...values,
+    // }));
 
-    const planet = await model.find({ planet: req.query.planet });
-    const planetSign = req.query.sign;
-    const now = dayjs();
-    const month = now.month();
+    // const planet = await model.find({ planet: req.query.planet });
+    // const planetSign = req.query.sign;
+    // const now = dayjs();
+    // const month = now.month();
 
-    const planetTransit = planet[0]?.transit;
-    const data = planetTransit?.find((w) => {
-      if (w.zodiac === planetSign) return w;
-    });
+    // const planetTransit = planet[0]?.transit;
+    // const data = planetTransit?.find((w) => {
+    //   if (w.zodiac === planetSign) return w;
+    // });
 
-    // if (typeof data === "undefined" || data.length == 0) {
-    //   return res
-    //     .status(404)
-    //     .json(`No transit this month for ${req.query.planet}`);
-    // }
+    // // if (typeof data === "undefined" || data.length == 0) {
+    // //   return res
+    // //     .status(404)
+    // //     .json(`No transit this month for ${req.query.planet}`);
+    // // }
+    // //---------------------------------------------
+    // const index = planetTransit?.indexOf(data);
+    // let secondIndex = index - 1;
+    // //---------------------------------------------
+    // let from = planetTransit[secondIndex]?.date;
+    // let to = planetTransit[index]?.date;
+
+    // let finaleData = dataArray.filter((x) => {
+    //   if (x.date <= to && x.date >= from) {
+    //     return x;
+    //   }
+    // });
+
+    // // console.log("Index", index);
+    // // console.log("secondIndex", secondIndex);
+    // console.log("finaleData", finaleData);
+
     //---------------------------------------------
-    const index = planetTransit.indexOf(data);
-    let secondIndex = index - 1;
-    //---------------------------------------------
-    let from = planetTransit[secondIndex]?.date;
-    let to = planetTransit[index]?.date;
-
-    let finaleData = dataArray.filter((x) => {
-      if (x.date <= to && x.date >= from) {
-        return x;
-      }
-    });
-
-    // console.log("Index", index);
-    // console.log("secondIndex", secondIndex);
-    console.log("finaleData", finaleData);
-
-    //---------------------------------------------
-    res.status(200).json(finaleData.reverse());
+    // res.status(200).json(finaleData.reverse());
+    res.status(200).json(chartData.data);
   },
 };

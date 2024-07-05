b6aad7b916c70940277b4404d8943913bad7e926
Nischal Gupta <nischal@onecorp.co.in>
2024-07-05 18:56:07 +0530

    planetary hours

--- constants/urls.js
+++ constants/urls.js
@@ -1,5 +1,5 @@
-// const ephemerisApi = "https://ephemeris.onrender.com"
-const ephemerisApi = "http://192.168.0.115:8000"
+const ephemerisApi = "https://ephemeris.onrender.com"
+// const ephemerisApi = "http://192.168.0.115:8000"
 
 
 module.exports = {ephemerisApi} 

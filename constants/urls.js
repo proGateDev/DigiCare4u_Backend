780903aeb7f73bcecc80368f344daa4646030dc7
Nischal Gupta <nischal@onecorp.co.in>
2024-08-02 18:27:44 +0530

    commit

--- constants/urls.js
+++ constants/urls.js
@@ -1,5 +1,5 @@
-// const ephemerisApi = "https://ephemeris.onrender.com"
-const ephemerisApi = "http://192.168.1.33:8000"
+const ephemerisApi = "https://ephemeris.onrender.com"
+// const ephemerisApi = "http://192.168.1.33:8000"
 
 
 module.exports = {ephemerisApi} 

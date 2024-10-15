42fa2c07716e6e06f69f45c2b09a736442b8ff12
DigiCareDev <rohit@progatetechnology.com>
2024-10-15 16:57:52 +0530

    commit

--- service/socket.js
+++ service/socket.js
@@ -15,7 +15,7 @@ const socketService = (server) => {
   io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
     socket.on('onLocationUpdate', async (data) => {
-      console.log('-------- onLocationUpdate  (SERVER)---------------', data);
+      console.log('-------- updating location in the background ---------------');
       const updatedMember = await memberModel.findByIdAndUpdate(
         '670df8794ac80f26f99dd7ee',
         {

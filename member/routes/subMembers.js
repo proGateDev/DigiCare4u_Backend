36a960888b8f8978b25804e7b080f9560687fb06
nischal <gupta.nischal014@gmail.com>
2025-01-24 15:32:47 +0530

    geo-fencing ...

--- member/routes/subMembers.js
+++ member/routes/subMembers.js
@@ -5,7 +5,13 @@ const checkUserToken = require("../../middleware/jwt");
 //==========================================
 
 router.get("/", checkUserToken, controller.getMemberSubUsers);     // Read
+router.get("/last-location", checkUserToken, controller.getMembersTeamLastLocation);     // Read
 
+router.get("/:teamMemberId", checkUserToken, controller.getMemberSubUsersById);     // Read
+router.get("/sos/:teamMemberId", checkUserToken, controller.sendSosToMemberById);     // Read
+router.get("/request-live-location/:teamMemberId", checkUserToken, controller.requestLiveLocationForTeamMemberById);     // Read
+router.post("/live-location-tracking-insight-report", checkUserToken, controller.fetchTeamMemberLiveLocationInsightReport);     // Read
+router.get("/assignments_/:startDate/:endDate/:teamMemberId", checkUserToken, controller.getMemberTeamAssignments);     // Read
 
 
 module.exports = router;
